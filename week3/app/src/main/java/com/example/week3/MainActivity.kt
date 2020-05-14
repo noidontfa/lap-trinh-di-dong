a77c7414a25fc54dfe0df4199edb0f5559513b4f
Bao Dien <baodien1405@gmail.com>
2020-05-14 12:21:20 +0700

    Update view detail

--- week3/app/src/main/java/com/example/week3/MainActivity.kt
+++ week3/app/src/main/java/com/example/week3/MainActivity.kt
@@ -1,12 +1,24 @@
 package com.example.week3
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.RatingBar
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
 
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_detail)
+        supportActionBar?.hide()
+
+        val rate = findViewById<View>(R.id.rating_bar) as RatingBar
+        val review = findViewById<Button>(R.id.btn_review) as Button
+        review.setOnClickListener(View.OnClickListener {
+            Toast.makeText(this, "Your rating:" + rate.rating.toString(), Toast.LENGTH_LONG).show()
+        })
     }
 }
