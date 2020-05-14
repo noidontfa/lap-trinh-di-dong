87eaa2e9ea93466ee2ec5c85322a93a345a3597a
trongphuoc <huynhtrongphuoc1698@gmail.com>
2020-05-14 16:24:50 +0700

    Movie Adapter

--- week3/app/src/main/java/com/example/week3/movie_adapter.kt
+++ week3/app/src/main/java/com/example/week3/movie_adapter.kt
@@ -1,2 +1,40 @@
 package com.example.week3
 
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextClock
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
+
+class MovieAdapter(val ctx : Context, val movies:MovieModel.Result) : RecyclerView.Adapter<MovieAdapter.MovieVH>(){
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MovieVH {
+        val view = LayoutInflater.from(ctx).inflate(R.layout.item_movie, parent,false)
+        return MovieVH(view)
+    }
+
+    override fun getItemCount(): Int {
+        return movies.results.size
+    }
+
+    override fun onBindViewHolder(holder: MovieVH, position: Int) {
+        val movie = movies.results[position]
+        Glide.with(ctx)
+            .load("https://image.tmdb.org/t/p/w500/"+movie.poster_path)
+            .centerCrop()
+            .into(holder.movie_avatar)
+        holder.movie_name.text = movie.title
+        holder.movie_desc.text = movie.overview
+
+    }
+
+    class MovieVH(itemView: View) : RecyclerView.ViewHolder(itemView){
+        val movie_avatar = itemView.findViewById<ImageView>(R.id.imageView2)
+        val movie_name = itemView.findViewById<TextView>(R.id.text_view_title)
+        val movie_desc = itemView.findViewById<TextView>(R.id.text_view_overview)
+    }
+}
