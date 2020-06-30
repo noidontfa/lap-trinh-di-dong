ddff99abb5046e0915742d19a8679eccab285ca8
“HoangSon” <dohoangsonqn@gmail.com>
2020-06-30 22:23:38 +0700

    create bottomNav | fragment map

---
+++ carparking/app/src/main/java/com/example/car_parking/LocationParking.kt
@@ -0,0 +1,291 @@
+
+package com.example.car_parking
+
+import android.Manifest
+import android.app.Activity
+import android.app.Application
+import android.content.Context
+import android.content.pm.ApplicationInfo
+import android.content.pm.PackageManager
+import android.graphics.Bitmap
+import android.graphics.Canvas
+import android.location.Location
+import android.os.Bundle
+import android.util.Log
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentActivity
+import androidx.fragment.app.FragmentManager
+import com.example.car_parking.models.MarkerModel
+import com.example.car_parking.models.RoomModel
+import com.google.android.gms.location.FusedLocationProviderClient
+import com.google.android.gms.location.LocationServices
+import com.google.android.gms.maps.CameraUpdateFactory
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.OnMapReadyCallback
+import com.google.android.gms.maps.SupportMapFragment
+import com.google.android.gms.maps.model.*
+import com.google.android.libraries.places.api.Places
+import com.google.android.libraries.places.api.net.PlacesClient
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
+import com.google.firebase.database.ValueEventListener
+import com.google.firebase.database.ktx.database
+import com.google.firebase.ktx.Firebase
+import java.util.*
+import kotlin.collections.ArrayList
+
+class LocationParking : Fragment(), OnMapReadyCallback, GoogleMap.OnMarkerClickListener {
+    val TAG = "MapsActivity"
+    private lateinit var mMap: GoogleMap
+    private val database = Firebase.database
+
+    private var cameraPosition: CameraPosition? = null
+
+    // The entry point to the Places API.
+    private lateinit var placesClient: PlacesClient
+
+    // The entry point to the Fused Location Provider.
+    private lateinit var fusedLocationProviderClient: FusedLocationProviderClient
+
+    // A default location (Sydney, Australia) and default zoom to use when location permission is
+    // not granted.
+    private val defaultLocation = LatLng(-33.8523341, 151.2106085)
+    private var locationPermissionGranted = false
+
+    // The geographical location where the device is currently located. That is, the last-known
+    // location retrieved by the Fused Location Provider.
+    private var lastKnownLocation: Location? = null
+
+//    override fun onCreate(savedInstanceState: Bundle?) {
+//        super.onCreate(savedInstanceState)
+//    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
+        val view: View = inflater.inflate(R.layout.fragment_location_parking, container, false)
+
+        // Retrieve location and camera position from saved instance state.
+        if (savedInstanceState != null) {
+            lastKnownLocation = savedInstanceState.getParcelable(KEY_LOCATION)
+            cameraPosition = savedInstanceState.getParcelable(KEY_CAMERA_POSITION)
+        }
+
+        // Construct a PlacesClient
+        Places.initialize(activity?.applicationContext!!, getString(R.string.google_api_key))
+        placesClient = Places.createClient(context as Activity)
+
+        // Construct a FusedLocationProviderClient.
+        fusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(context as Activity)
+
+        val mapFragment = childFragmentManager.findFragmentById(R.id.map) as? SupportMapFragment
+        mapFragment?.getMapAsync(this)
+
+        return view
+    }
+
+    /**
+     * Saves the state of the map when the activity is paused.
+     */
+    override fun onSaveInstanceState(outState: Bundle) {
+        mMap?.let { map ->
+            outState.putParcelable(KEY_CAMERA_POSITION, map.cameraPosition)
+            outState.putParcelable(KEY_LOCATION, lastKnownLocation)
+        }
+        super.onSaveInstanceState(outState)
+    }
+
+    /**
+     * Manipulates the map once available.
+     * This callback is triggered when the map is ready to be used.
+     * This is where we can add markers or lines, add listeners or move the camera. In this case,
+     * we just add a marker near Sydney, Australia.
+     * If Google Play services is not installed on the device, the user will be prompted to install
+     * it inside the SupportMapFragment. This method will only be triggered once the user has
+     * installed Google Play services and returned to the app.
+     */
+    override fun onMapReady(googleMap: GoogleMap) {
+        mMap = googleMap
+        onLoadMarkerFromDatabase(mMap)
+        mMap.setOnMarkerClickListener {marker ->
+            onLoadRoomFromDatabase(marker)
+            true
+        }
+        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(LatLng(10.7629183,106.679983),
+            DEFAULT_ZOOM))
+
+        mMap.setOnMarkerClickListener(this)
+
+        // Prompt the user for permission.
+        getLocationPermission()
+
+        // Turn on the My Location layer and the related control on the map.
+        updateLocationUI()
+        // Get the current location of the device and set the position of the map.
+        getDeviceLocation()
+    }
+
+    private fun updateLocationUI() {
+        if (mMap == null) {
+            return
+        }
+        try {
+            if (locationPermissionGranted) {
+                mMap?.isMyLocationEnabled = true
+                mMap?.uiSettings?.isMyLocationButtonEnabled = true
+            } else {
+                mMap?.isMyLocationEnabled = false
+                mMap?.uiSettings?.isMyLocationButtonEnabled = false
+                lastKnownLocation = null
+                getLocationPermission()
+            }
+        } catch (e: SecurityException) {
+            Log.e("Exception: %s", e.message, e)
+        }
+    }
+
+    private fun getLocationPermission() {
+         /*
+         * Request location permission, so that we can get the location of the
+         * device. The result of the permission request is handled by a callback,
+         * onRequestPermissionsResult.
+         */
+        if (ActivityCompat.checkSelfPermission(activity?.applicationContext!!,
+                Manifest.permission.ACCESS_FINE_LOCATION)
+            == PackageManager.PERMISSION_GRANTED) {
+            locationPermissionGranted = true
+        } else {
+            ActivityCompat.requestPermissions(context as Activity, arrayOf(Manifest.permission.ACCESS_FINE_LOCATION),
+                PERMISSIONS_REQUEST_ACCESS_FINE_LOCATION)
+//            targetFragment?.requestPermissions(arrayOf(Manifest.permission.ACCESS_FINE_LOCATION),
+//                PERMISSIONS_REQUEST_ACCESS_FINE_LOCATION)
+1        }
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        locationPermissionGranted = false
+        when (requestCode){
+            PERMISSIONS_REQUEST_ACCESS_FINE_LOCATION->{
+                if (grantResults.isNotEmpty() &&
+                    grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    locationPermissionGranted = true
+                }
+            }
+        }
+        updateLocationUI()
+    }
+
+    /**
+     * Gets the current location of the device, and positions the map's camera.
+     */
+    private fun getDeviceLocation() {
+        /*
+         * Get the best and most recent location of the device, which may be null in rare
+         * cases when a location is not available.
+         */
+        try {
+            if (locationPermissionGranted) {
+                val locationResult = fusedLocationProviderClient.lastLocation
+                locationResult.addOnCompleteListener(Activity()) { task ->
+                    if (task.isSuccessful) {
+                        // Set the map's camera position to the current location of the device.
+                        lastKnownLocation = task.result
+                        if (lastKnownLocation != null) {
+                            mMap?.moveCamera(CameraUpdateFactory.newLatLngZoom(
+                                LatLng(lastKnownLocation!!.latitude,
+                                    lastKnownLocation!!.longitude), DEFAULT_ZOOM.toFloat()))
+                        }
+                    } else {
+                        Log.d(TAG, "Current location is null. Using defaults.")
+                        Log.e(TAG, "Exception: %s", task.exception)
+                        mMap?.moveCamera(CameraUpdateFactory
+                            .newLatLngZoom(defaultLocation, DEFAULT_ZOOM.toFloat()))
+                        mMap?.uiSettings?.isMyLocationButtonEnabled = false
+                    }
+                }
+            }
+        } catch (e: SecurityException) {
+            Log.e("Exception: %s", e.message, e)
+        }
+    }
+
+    private fun onLoadRoomFromDatabase(marker: Marker) {
+        val roomId = marker.tag
+        database.getReference("rooms").orderByChild("roomId").equalTo(roomId.toString())
+            .addValueEventListener(object : ValueEventListener {
+                override fun onCancelled(error: DatabaseError) {
+                    Log.d(TAG,error.toString())
+                }
+                override fun onDataChange(snapshot: DataSnapshot) {
+                    for ( snap in snapshot.children) {
+                        val value = snap.getValue(RoomModel::class.java)
+                        Log.d(TAG,value.toString())
+                        Toast.makeText(activity,value!!.name,Toast.LENGTH_SHORT).show()
+                        // xu ly man hinh car parking o day
+                    }
+                }
+
+            })
+    }
+
+    private fun bitmapDescriptorFromVector(context: Context, vectorResId: Int): BitmapDescriptor? {
+        return ContextCompat.getDrawable(context, vectorResId)?.run {
+            setBounds(0, 0, intrinsicWidth, intrinsicHeight)
+            val bitmap = Bitmap.createBitmap(intrinsicWidth, intrinsicHeight, Bitmap.Config.ARGB_8888)
+            draw(Canvas(bitmap))
+            BitmapDescriptorFactory.fromBitmap(bitmap)
+        }
+    }
+
+    private fun onLoadMarkerFromDatabase(mMap : GoogleMap) {
+        val markerRef = database.getReference("marker")
+        markerRef.addValueEventListener(object : ValueEventListener {
+            override fun onCancelled(error: DatabaseError) {
+                Log.d(TAG,error.toString())
+            }
+            override fun onDataChange(snapshot: DataSnapshot) {
+                for (snap in snapshot.children) {
+                    val marker = snap.getValue(MarkerModel::class.java)
+                    val position = LatLng(marker!!.x,marker!!.y)
+                    val m = mMap.addMarker(MarkerOptions().position(position).title(marker.title)
+                        .icon(bitmapDescriptorFromVector(activity?.applicationContext!!,R.drawable.test)))
+                    m.tag = marker.roomId
+                }
+            }
+        })
+    }
+
+    override fun onMarkerClick(mMarker: Marker?): Boolean {
+        val polyline1 = mMap.addPolyline(PolylineOptions()
+            .clickable(true)
+            .add(
+                LatLng(10.7475533,106.6882773),
+                LatLng(10.7628339,106.6802727)))
+        // Store a data object with the polyline, used here to indicate an arbitrary type.
+        polyline1.tag = "A"
+        return false
+    }
+
+    companion object {
+        private const val DEFAULT_ZOOM = 17.0f
+        private const val PERMISSIONS_REQUEST_ACCESS_FINE_LOCATION = 1
+
+        // Keys for storing activity state.
+        private const val KEY_CAMERA_POSITION = "camera_position"
+        private const val KEY_LOCATION = "location"
+    }
+}
--- carparking/app/src/main/java/com/example/car_parking/MapsActivity.kt
+++ carparking/app/src/main/java/com/example/car_parking/MapsActivity.kt
@@ -5,9 +5,12 @@ import android.graphics.Bitmap
 import android.graphics.Canvas
 import android.os.Bundle
 import android.util.Log
+import android.view.MenuItem
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.content.ContextCompat
+import androidx.fragment.app.FragmentTransaction
+import androidx.lifecycle.Transformations.map
 import com.example.car_parking.models.MarkerModel
 import com.example.car_parking.models.RoomModel
 import com.google.android.gms.maps.CameraUpdateFactory
@@ -15,6 +18,7 @@ import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.OnMapReadyCallback
 import com.google.android.gms.maps.SupportMapFragment
 import com.google.android.gms.maps.model.*
+import com.google.android.material.bottomnavigation.BottomNavigationView
 import com.google.firebase.database.DataSnapshot
 import com.google.firebase.database.DatabaseError
 import com.google.firebase.database.ValueEventListener
@@ -23,85 +27,60 @@ import com.google.firebase.ktx.Firebase
 import java.util.*
 import kotlin.collections.ArrayList
 
-class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
+class MapsActivity : AppCompatActivity(){
     val TAG = "MapsActivity"
     private lateinit var mMap: GoogleMap
     private val database = Firebase.database
+
+    lateinit var locationParking: LocationParking
+    lateinit var parking: Parking
+    lateinit var account: Account
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_maps)
         // Obtain the SupportMapFragment and get notified when the map is ready to be used.
-        val mapFragment = supportFragmentManager
-                .findFragmentById(R.id.map) as SupportMapFragment
-        mapFragment.getMapAsync(this)
+//        val mapFragment = supportFragmentManager
+//                .findFragmentById(R.id.map) as SupportMapFragment
+//        mapFragment.getMapAsync(this)
+        locationParking = LocationParking()
+        supportFragmentManager
+            .beginTransaction()
+            .replace(R.id.fragmentContainer, locationParking)
+            .setTransition(FragmentTransaction.TRANSIT_FRAGMENT_OPEN)
+            .commit()
 
-    }
-
-    /**
-     * Manipulates the map once available.
-     * This callback is triggered when the map is ready to be used.
-     * This is where we can add markers or lines, add listeners or move the camera. In this case,
-     * we just add a marker near Sydney, Australia.
-     * If Google Play services is not installed on the device, the user will be prompted to install
-     * it inside the SupportMapFragment. This method will only be triggered once the user has
-     * installed Google Play services and returned to the app.
-     */
-    override fun onMapReady(googleMap: GoogleMap) {
-        mMap = googleMap
-        onLoadMarkerFromDatabase(mMap)
-        mMap.setOnMarkerClickListener {marker ->
-            onLoadRoomFromDatabase(marker)
+        val bottomNav : BottomNavigationView = findViewById(R.id.bottomNav)
+        bottomNav.setOnNavigationItemSelectedListener { item: MenuItem ->
+            when(item.itemId){
+                R.id.locationParking -> {
+                    locationParking = LocationParking()
+                    supportFragmentManager
+                        .beginTransaction()
+                        .replace(R.id.fragmentContainer, locationParking)
+                        .setTransition(FragmentTransaction.TRANSIT_FRAGMENT_OPEN)
+                        .commit()
+                }
+                R.id.Parking -> {
+                    parking = Parking()
+                    supportFragmentManager
+                        .beginTransaction()
+                        .replace(R.id.fragmentContainer, parking)
+                        .setTransition(FragmentTransaction.TRANSIT_FRAGMENT_OPEN)
+                        .commit()
+                }
+                R.id.Account -> {
+                    account = Account()
+                    supportFragmentManager
+                        .beginTransaction()
+                        .replace(R.id.fragmentContainer, account)
+                        .setTransition(FragmentTransaction.TRANSIT_FRAGMENT_OPEN)
+                        .commit()
+                }
+            }
             true
         }
-        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(LatLng(10.7629183,106.679983),17.0f))
     }
-
-    private fun onLoadRoomFromDatabase(marker: Marker) {
-        val roomId = marker.tag
-        database.getReference("rooms").orderByChild("roomId").equalTo(roomId.toString())
-            .addValueEventListener(object : ValueEventListener {
-                override fun onCancelled(error: DatabaseError) {
-                    Log.d(TAG,error.toString())
-                }
-                override fun onDataChange(snapshot: DataSnapshot) {
-                    for ( snap in snapshot.children) {
-                        val value = snap.getValue(RoomModel::class.java)
-                        Log.d(TAG,value.toString())
-                        Toast.makeText(applicationContext,value!!.name,Toast.LENGTH_SHORT).show()
-                        // xu ly man hinh car parking o day
-                    }
-                }
-
-            })
-    }
-
-    private fun bitmapDescriptorFromVector(context: Context, vectorResId: Int): BitmapDescriptor? {
-        return ContextCompat.getDrawable(context, vectorResId)?.run {
-            setBounds(0, 0, intrinsicWidth, intrinsicHeight)
-            val bitmap = Bitmap.createBitmap(intrinsicWidth, intrinsicHeight, Bitmap.Config.ARGB_8888)
-            draw(Canvas(bitmap))
-            BitmapDescriptorFactory.fromBitmap(bitmap)
-        }
-    }
-
-    private fun onLoadMarkerFromDatabase(mMap : GoogleMap) {
-            val markerRef = database.getReference("marker")
-            markerRef.addValueEventListener(object : ValueEventListener {
-                override fun onCancelled(error: DatabaseError) {
-                    Log.d(TAG,error.toString())
-                }
-                override fun onDataChange(snapshot: DataSnapshot) {
-                    for (snap in snapshot.children) {
-                        val marker = snap.getValue(MarkerModel::class.java)
-                        val position = LatLng(marker!!.x,marker!!.y)
-                        val m = mMap.addMarker(MarkerOptions().position(position).title(marker.title)
-                            .icon(bitmapDescriptorFromVector(this@MapsActivity,R.drawable.test)))
-                        m.tag = marker.roomId
-                    }
-                }
-            })
-        }
-
 }
 
 
