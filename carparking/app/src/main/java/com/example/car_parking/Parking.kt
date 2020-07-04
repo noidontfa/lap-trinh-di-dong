494a5bd87fc8a8b0b52cc830c48f88dceb953eaa
Bao Dien <baodien1405@gmail.com>
2020-07-04 19:41:46 +0700

    Update demo praking fragment

--- carparking/app/src/main/java/com/example/car_parking/Parking.kt
+++ carparking/app/src/main/java/com/example/car_parking/Parking.kt
@@ -1,15 +1,21 @@
 package com.example.car_parking
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.SeekBar
+import android.widget.TextView
+import androidx.fragment.app.Fragment
+import kotlinx.android.synthetic.*
 
 /**
  * A simple [Fragment] subclass.
  */
 class Parking : Fragment() {
+    lateinit var slider: SeekBar
+    lateinit var valueTime: TextView
+    lateinit var totalPay: TextView
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -17,6 +23,31 @@ class Parking : Fragment() {
     ): View? {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_parking, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        slider = view.findViewById(R.id.seekBar) as SeekBar
+        valueTime = view.findViewById(R.id.textView16) as TextView
+        totalPay = view.findViewById(R.id.textView15) as TextView
+
+        slider.max = 10
+
+        slider.setOnSeekBarChangeListener(object : SeekBar.OnSeekBarChangeListener{
+            override fun onProgressChanged(seekBar: SeekBar?, progress: Int, fromUser: Boolean) {
+                valueTime.text = progress.toString()
+            }
+
+            override fun onStartTrackingTouch(seekBar: SeekBar?) {
+                valueTime.text = seekBar?.progress.toString()
+            }
+
+            override fun onStopTrackingTouch(seekBar: SeekBar?) {
+                valueTime.text = seekBar?.progress.toString()
+                totalPay.text = (seekBar!!.progress*10000.toInt()).toString()
+            }
+        })
     }
 
 }
---
+++ week4/app/src/main/java/com/example/week4/MyApplication.kt
@@ -0,0 +1,4 @@
+package com.example.week4
+
+class MyApplication {
+}
