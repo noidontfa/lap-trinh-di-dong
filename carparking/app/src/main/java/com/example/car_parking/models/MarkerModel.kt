aa51316f10b75695bb38be3dfd35aa5399dd8465
trantamsn98 <trantamsn98@gmail.com>
2020-07-10 22:44:23 +0700

    Popup updated

--- carparking/app/src/main/java/com/example/car_parking/models/MarkerModel.kt
+++ carparking/app/src/main/java/com/example/car_parking/models/MarkerModel.kt
@@ -1,5 +1,5 @@
 package com.example.car_parking.models
 
-data class MarkerModel(var x: Double, var y: Double, var title: String, var roomId: String) {
-    constructor() : this(0.0,0.0,"","")
+data class MarkerModel(var x: Double, var y: Double, var title: String, var roomId: String, var address: String, var hour: String, var rating: Double) {
+    constructor() : this(0.0,0.0,"","","","",0.0)
 }
