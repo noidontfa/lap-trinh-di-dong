f08e132358dd843dfeb2033c068f792e1217169d
trantamsn98 <trantamsn98@gmail.com>
2020-07-10 22:11:35 +0700

    Update popup

--- carparking/app/src/main/java/com/example/car_parking/models/PopupParking.kt
+++ carparking/app/src/main/java/com/example/car_parking/models/PopupParking.kt
@@ -1,11 +1,21 @@
 package com.example.car_parking
 
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
 import androidx.fragment.app.Fragment
+import com.example.car_parking.models.MarkerModel
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
+import com.google.firebase.database.ValueEventListener
+import com.google.firebase.database.ktx.database
+import com.google.firebase.ktx.Firebase
 
+val TAG = "MapsActivity"
+private val database = Firebase.database
 
 class PopupParking : Fragment() {
     override fun onCreateView(
@@ -16,4 +26,31 @@ class PopupParking : Fragment() {
         val view: View? = inflater?.inflate(R.layout.fragment_popup_info, container, false)
         return view
     }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        val tvTitle: TextView = view.findViewById(R.id.tv_title)
+        val tvAddr: TextView = view.findViewById(R.id.tv_addr)
+        val tvHour: TextView = view.findViewById(R.id.tv_hour)
+        val tvRating: TextView = view.findViewById(R.id.tv_rating)
+        val markerRef = database.getReference("marker")
+        markerRef.addValueEventListener(object : ValueEventListener {
+            override fun onDataChange(dataSnapshot: DataSnapshot) {
+                for (snap in dataSnapshot.children) {
+                    val marker = snap.getValue(MarkerModel::class.java)
+                    tvTitle.setText(marker!!.title)
+                    tvAddr.setText(marker!!.address)
+                    tvHour.setText(marker!!.hour)
+                    tvRating.setText(marker!!.rating.toString())
+                }
+            }
+
+            override fun onCancelled(databaseError: DatabaseError) {
+                // Getting Post failed, log a message
+                Log.w(TAG, "loadPost:onCancelled", databaseError.toException())
+                // ...
+            }
+        })
+    }
+
 }
