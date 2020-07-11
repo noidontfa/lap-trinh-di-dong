5a151dc04d5507bb104c57d061bc31228540a992
trongphuoc <huynhtrongphuoc1698@gmail.com>
2020-07-11 14:52:42 +0700

    Slot Model

--- carparking/app/src/main/java/com/example/car_parking/models/SlotModel.kt
+++ carparking/app/src/main/java/com/example/car_parking/models/SlotModel.kt
@@ -1,3 +1,6 @@
 package com.example.car_parking.models
 
-data class SlotModel(var ParkingFee : String, var LicensePlate: String, var ParkingTime: String)
+data class SlotModel(var ParkingFee : String, var LicensePlate: String, var ParkingTime: String, var isEmpty: Boolean, var roomId : String)
+{
+    constructor() : this("","", "", false, "")
+}
